3a1a359c089991e861e9b7f053d26f2e2e311505
Evan <wanghaitang.ufl@gmail.com>
2017-10-29 17:52:02 -0400

    find nodes in the negative cycles

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -3,29 +3,29 @@
 #include <vector>
 #include <queue>
 #include <functional>
-
+#include <climits>
 using std::vector;
 using std::queue;
 using std::pair;
 using std::priority_queue;
 using std::make_pair;
 using std::greater;
-typedef pair<int, int> iPair;
+typedef pair<int, long long> iPair;
 
 void shortest_paths(vector<vector<iPair> > &adj, int s, vector<long long> &distance, vector<int> &reachable, vector<int> &shortest) {
-	
+
 	distance[s] = 0;
 	int n = adj.size();
-	
+
 	vector<iPair>::iterator it;
 	/*for (it = adj[s].begin(); it != adj[s].end(); it++)
 	{
-		int v = (*it).first;
-		int weight = (*it).second;
-		if (distance[v] > distance[s] + weight)
-			distance[v] = distance[s] + weight;
+	int v = (*it).first;
+	int weight = (*it).second;
+	if (distance[v] > distance[s] + weight)
+	distance[v] = distance[s] + weight;
 	}
-*/
+	*/
 	queue<int> neg_que;
 	for (int i = 1; i <n; i++)
 	{
@@ -37,14 +37,11 @@ void shortest_paths(vector<vector<iPair> > &adj, int s, vector<long long> &dista
 				for (it = adj[u].begin(); it != adj[u].end(); it++)
 				{
 					int v = (*it).first;
-					int weight = (*it).second;
+					long long weight = (*it).second;
 					if (distance[v] > distance[u] + weight)
 						distance[v] = distance[u] + weight;
-
-					
-
 				}
-			}			
+			}
 		}
 	}
 
@@ -64,16 +61,16 @@ void shortest_paths(vector<vector<iPair> > &adj, int s, vector<long long> &dista
 			{
 				int v = (*it).first;
 				int weight = (*it).second;
-				if ( distance[v] > distance[u] + weight)
+				if (distance[v] > distance[u] + weight)
 				{
-					neg_que.push(v);		
+					neg_que.push(v);
 					distance[v] = distance[u] + weight;
 				}
 				reachable[v] = 1;
 			}
 			reachable[u] = 1;
 		}
-		
+
 	}
 
 	while (!neg_que.empty())
@@ -88,16 +85,21 @@ void shortest_paths(vector<vector<iPair> > &adj, int s, vector<long long> &dista
 			{
 				int v = (*it).first;
 				int weight = (*it).second;
-				if (distance[v] > distance[u] + weight && shortest[v]!=0)
+				if (distance[v] > distance[u] + weight && shortest[v] != 0)
 				{
 					neg_que.push(v);
 					distance[v] = distance[u] + weight;
 					//std::cout << "did it haoppp?";
 				}
+				if (v == s)
+				{
+					shortest.assign(n, 0);
+					break;
+				}
 			}
-		}			
+		}
 	}
-	
+
 }
 
 int main() {
@@ -107,7 +109,8 @@ int main() {
 	//vector<vector<int> > cost(n, vector<int>());
 	vector<vector<iPair> > adj(n, vector<iPair>());
 	for (int i = 0; i < m; i++) {
-		int x, y, w;
+		int x, y;
+		long long w;
 		std::cin >> x >> y >> w;
 		//adj[x - 1].push_back(y - 1);
 		//cost[x - 1].push_back(w);
